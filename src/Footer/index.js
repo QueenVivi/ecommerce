aeb254e02f0822dc8fed90309feb09ad4570ee5b
Vivian Xu <hi@vivianxu.com>
2023-08-12 21:56:48 +1000

    Footer and ProductDetail markup

---
+++ src/Footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  const year = new Date().getFullYear()
+  
+  return (
+    <>
+      <h2>Northern Store</h2>
+      <p>{year} the New Bohemian Store </p>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/ProductDetail/index.js
@@ -0,0 +1,9 @@
+const ProductDetail = () => {
+  return (
+    <>
+    <h1>earrings</h1>
+    </>
+  )
+}
+
+export default ProductDetail
