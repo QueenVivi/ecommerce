321a199ffa4fcc51a2e2939b8e8608fbcf908fdd
Vivian Xu <hi@vivianxu.com>
2023-08-13 11:46:38 +1000

    Get product list data into app

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,20 +1,51 @@
 import Stripe from "stripe"
+import formatPrice from "../../lib/formatPrice"
 
 const stripe = new Stripe(process.env.REACT_APP_API_KEY)
 
-const products = await stripe.products.list({
+const { data: productsData } = await stripe.products.list({
   limit: 10,
 })
 
+const { data: pricesData } = await stripe.prices.list({
+  limit: 10,
+})
+
+const products = productsData.map((productData) => {
+  const {
+    id,
+    name,
+    description,
+    default_price,
+    images,
+    metadata,
+  } = productData
+
+  const { unit_amount } = pricesData.find((priceData) => priceData.id === default_price)
+
+  const price = unit_amount / 100
+
+  return {
+    id,
+    name,
+    price,
+    description,
+    images,
+    metadata,
+  }
+})
+
 const ProductList = () => {
-  const { data: productsData } = products
-  console.log(productsData)
   return (
     <ol>
-      {productsData.map((product) => {
-        const { id, images, name, description, price, metadata } = product
+      {products.map((product) => {
+        const { id, images, name, price, metadata } = product
         return (
-          <li key={id}>{name}</li>
+          <li key={id}>
+            <h3>{name}</h3>
+            <img alt={name} src={images[0]} />
+            {formatPrice(price)}
+          </li>
         )
       })}
     </ol>
