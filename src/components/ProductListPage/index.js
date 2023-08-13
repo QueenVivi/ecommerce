6e313601131a20abdcf5e170cf4b59e15e63e8e8
Vivian Xu <hi@vivianxu.com>
2023-08-13 22:32:38 +1000

    Set up filter

--- src/components/ProductListPage/index.js
+++ src/components/ProductListPage/index.js
@@ -5,6 +5,7 @@ import formatPrice from "../../lib/formatPrice"
 
 const ProductListPage = () => {
   const [ products, setProducts ] = useState(null)
+  const [ category, setCategory ] = useState("")
 
   useEffect(() => {
     const fetchAllProducts = async () => {
@@ -23,22 +24,41 @@ const ProductListPage = () => {
     )
   }
 
-  return (
-    <ol className="grid grid-rows-3 gap-4" >
-      {products.map((product) => {
-        const { id, images, name, price, metadata } = product
+  const filteredProducts = category 
+    ? products.filter((product) => category === product.metadata.category)
+    : products
 
-        return (
-          <li key={id}>
-            <Link to={`/product/${id}`}>
-              <h3>{name}</h3>
-              <img alt={name} src={images[0]} />
-              {formatPrice(price)}
-            </Link>
-          </li>
+  return (
+    <section>
+      <select onChange={(e) => setCategory(e.target.value)}>
+        <option value="">All</option>
+        <option value="crystal">Crystals</option>
+        <option value="earring">Earrings</option>
+        <option value="necklace">Necklaces</option>
+      </select>
+      {
+        filteredProducts.length > 0
+        ? (
+          <ol className="grid grid-rows-3 gap-4" >
+            {filteredProducts.map((product) => {
+              const { id, images, name, price, metadata } = product
+
+              return (
+                <li key={id}>
+                  <Link to={`/product/${id}`}>
+                    <h3>{name}</h3>
+                    <img alt={name} src={images[0]} />
+                    {formatPrice(price)}
+                  </Link>
+                </li>
+              )
+            })}
+          </ol>
+        ) : (
+          <p>Sorry, no match</p>
         )
-      })}
-    </ol>
+      }
+    </section>
   )
 }
 
